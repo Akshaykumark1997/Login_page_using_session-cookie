03fb2c95ba44dfc253a54522f6e05737ca09bd8d
Akshay kumar k <akshayajith40@gmail.com>
2022-10-25 14:59:06 +0530

    modified cache control

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ app.post('/login',(req,res)=>{
         res.render('index',{err_message:"username or password incorrect"});
     }
 });
+app.use((req, res, next) => {
+    res.header(
+      "Cache-Control",
+      "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+    );
+    next();
+  });
 app.get('/home',(req,res)=>{
     session=req.session;
     if(session.username){
