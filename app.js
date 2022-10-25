5cd2e831ed89e84269caf7a07c4b2337d25ea4d7
Akshay kumar k <akshayajith40@gmail.com>
2022-10-25 09:37:38 +0530

    login and home page using session and cookie

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require("path"); 
+const sessions = require('express-session');
+const cookieParser = require('cookie-parser');  
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine','ejs');
+app.listen(8000);
+app.use(express.static("public"));
+app.use("/image", express.static(path.join(__dirname + "public/image")));
+app.set(cookieParser());
+app.use(sessions({
+    secret:"123",
+    resave:true,
+    saveUninitialized:true,
+    cookie: {maxAge:30000},
+}));
+
+
+
+let session;
+const loginDetails = {
+    username:"akshay",
+    password:"123",
+};
+
+app.get('/',(req,res)=>{
+    session=req.session;
+    if(session.username){
+        res.render('index_home');
+    }else{
+        res.render('index');
+    }
+});
+app.post('/login',(req,res)=>{
+    if(req.body.username===loginDetails.username&&req.body.password===loginDetails.password){
+        session = req.session;
+        session.username = req.body.username;
+        res.render('index_home');
+    }else{
+        res.render('index',{err_message:"username or password incorrect"});
+    }
+});
+app.get('/logout',(req,res)=>{
+    req.session.destroy();
+    res.render('index');
+})
+
+
